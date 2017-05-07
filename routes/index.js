a6389c70772cb923aee45a80c6738c39581a5a0c
Duncan Mackenzie <duncanma@microsoft.com>
2017-05-06 20:41:28 -0700

    qualify app call

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  app.locals.insights.trackEvent("www.index");
+  req.app.locals.insights.trackEvent("www.index");
 
   res.render('index', { title: 'Seattle Meetup Demo' });
 });
